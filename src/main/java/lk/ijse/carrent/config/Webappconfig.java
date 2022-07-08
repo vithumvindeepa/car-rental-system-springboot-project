4cf855e66e0e44e2d4c4e040d4250291e3c19f09
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-09 00:48:11 +0530

    update

--- src/main/java/lk/ijse/carrent/config/Webappconfig.java
+++ src/main/java/lk/ijse/carrent/config/Webappconfig.java
@@ -1,5 +1,18 @@
 package lk.ijse.carrent.config;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.carrent")
+@EnableWebMvc
 public class Webappconfig {
 
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
+  }
 }
