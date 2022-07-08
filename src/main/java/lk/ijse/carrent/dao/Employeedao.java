8f1eb84e1603a21ab154e86b6f0241074c9d2bd9
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-09 01:25:27 +0530

    update

--- src/main/java/lk/ijse/carrent/dao/Employeedao.java
+++ src/main/java/lk/ijse/carrent/dao/Employeedao.java
@@ -1,11 +1,21 @@
 package lk.ijse.carrent.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+
 public class Employeedao {
 String EmployeeID;
 String Name;
 int ContactNo;
 String Email;
 String EmployeeType;
-Double Salary;
+double Salary;
 
 }
--- src/main/java/lk/ijse/carrent/entity/Employee.java
+++ src/main/java/lk/ijse/carrent/entity/Employee.java
@@ -1,5 +1,25 @@
 package lk.ijse.carrent.entity;
 
-public class Employee {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
+public class Employee {
+    @Id
+    String EmployeeID;
+    String Name;
+    int ContactNo;
+    String Email;
+    String EmployeeType;
+    double Salary;
 }
