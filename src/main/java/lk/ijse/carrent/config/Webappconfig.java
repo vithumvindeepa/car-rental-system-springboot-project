2fa90e20cfd5413fe81700a607217643eae1af76
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-09 00:49:12 +0530

    update

--- src/main/java/lk/ijse/carrent/config/Webappconfig.java
+++ src/main/java/lk/ijse/carrent/config/Webappconfig.java
@@ -4,6 +4,8 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
@@ -15,4 +17,8 @@ public class Webappconfig {
     public ModelMapper modelMapper(){
         return new ModelMapper();
   }
+    @Bean
+    public MultipartResolver multipartResolver(){
+        return new StandardServletMultipartResolver();
+    }
 }
