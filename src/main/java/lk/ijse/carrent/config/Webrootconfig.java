0f145a4b905d4c7fd65279b3e4ca28a74406d4eb
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-09 00:51:40 +0530

    update

--- src/main/java/lk/ijse/carrent/config/Webrootconfig.java
+++ src/main/java/lk/ijse/carrent/config/Webrootconfig.java
@@ -1,5 +1,10 @@
 package lk.ijse.carrent.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+@Configuration
+@Import(JPAconfig.class)
 public class Webrootconfig {
 
 }
