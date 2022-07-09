48d3f43cdcb5c2ed46656a095bc608127f36c607
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-10 00:13:22 +0530

    created Booking entity class

--- src/main/java/lk/ijse/carrent/entity/Booking.java
+++ src/main/java/lk/ijse/carrent/entity/Booking.java
@@ -1,7 +1,21 @@
 package lk.ijse.carrent.entity;
 
-public class Booking {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
+public class Booking {
+    @Id
     String BookingID;
     String Date;
     String PickupLocation;
