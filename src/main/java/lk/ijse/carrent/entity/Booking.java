84e52cf87d180c31afc9b5d5c92e345a401c6103
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-10 00:09:55 +0530

    created Booking entity class

--- src/main/java/lk/ijse/carrent/entity/Booking.java
+++ src/main/java/lk/ijse/carrent/entity/Booking.java
@@ -2,5 +2,11 @@ package lk.ijse.carrent.entity;
 
 public class Booking {
 
+    String BookingID;
+    String Date;
+    String PickupLocation;
+    double AdvanceAmount;
+    String VehiclePlateNo;
+    String CustomerName;
 
 }
