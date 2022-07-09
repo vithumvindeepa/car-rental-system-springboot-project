8087872c6fa0465b13e6a84066f1b2c6f3ffa315
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-10 00:08:46 +0530

    created Bookingdao class

--- src/main/java/lk/ijse/carrent/dao/Bookingdao.java
+++ src/main/java/lk/ijse/carrent/dao/Bookingdao.java
@@ -1,5 +1,15 @@
 package lk.ijse.carrent.dao;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+
 public class Bookingdao {
 
 String BookingID;
