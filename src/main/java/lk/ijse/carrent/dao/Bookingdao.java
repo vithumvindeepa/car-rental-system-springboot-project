7e992ee4f044733c79eaba6a254e0f95673113a6
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-10 00:07:15 +0530

    created Bookingdao class

--- src/main/java/lk/ijse/carrent/dao/Bookingdao.java
+++ src/main/java/lk/ijse/carrent/dao/Bookingdao.java
@@ -2,5 +2,11 @@ package lk.ijse.carrent.dao;
 
 public class Bookingdao {
 
+String BookingID;
+String Date;
+String PickupLocation;
+double AdvanceAmount;
+String VehiclePlateNo;
+String CustomerName;
 
 }
