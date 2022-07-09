45b21cbbd151217d2a34490a6d240acb8c80533a
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-09 20:57:10 +0530

    created Billing class

--- src/main/java/lk/ijse/carrent/entity/Billing.java
+++ src/main/java/lk/ijse/carrent/entity/Billing.java
@@ -2,5 +2,11 @@ package lk.ijse.carrent.entity;
 
 public class Billing {
 
+    String BillID;
+    String BookingID;
+    String BillDate;
+    double AmountBalance;
+    double AdvanceAmount;
+    double DamageCost;
 
 }
