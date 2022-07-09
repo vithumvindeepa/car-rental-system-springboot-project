dbf6d7fd6e064f61c3b177dd1e5ed54171562ad2
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-09 20:58:12 +0530

    created Billing class

--- src/main/java/lk/ijse/carrent/entity/Billing.java
+++ src/main/java/lk/ijse/carrent/entity/Billing.java
@@ -1,7 +1,21 @@
 package lk.ijse.carrent.entity;
 
-public class Billing {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
+public class Billing {
+    @Id
     String BillID;
     String BookingID;
     String BillDate;
