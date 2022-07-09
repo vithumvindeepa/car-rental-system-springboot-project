c5b753c7fff476ad6af7d368fe051f1c83eb7d63
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-09 20:18:56 +0530

    creatdriverentity

--- src/main/java/lk/ijse/carrent/entity/Driver.java
+++ src/main/java/lk/ijse/carrent/entity/Driver.java
@@ -1,7 +1,21 @@
 package lk.ijse.carrent.entity;
 
-public class Driver {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
+public class Driver {
+    @Id
 String DriverID;
 String DriverName;
 int ContactNo;
