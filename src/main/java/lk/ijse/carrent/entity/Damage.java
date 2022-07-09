f2e6946a761eee3175bc8797f2b61ee1e8c73159
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-10 00:21:46 +0530

    created Damage entity class

--- src/main/java/lk/ijse/carrent/entity/Damage.java
+++ src/main/java/lk/ijse/carrent/entity/Damage.java
@@ -1,7 +1,22 @@
 package lk.ijse.carrent.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+@Entity
+
 public class Damage {
 
+    @Id
     String DamageID;
     String VehicleID;
     String VehiclePlateNo;
