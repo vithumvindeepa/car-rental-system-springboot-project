75ac3b0b55aad2c929fd7e3e18103ced70d99619
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-10 00:20:18 +0530

    created Damage entity class

--- src/main/java/lk/ijse/carrent/entity/Damage.java
+++ src/main/java/lk/ijse/carrent/entity/Damage.java
@@ -2,5 +2,11 @@ package lk.ijse.carrent.entity;
 
 public class Damage {
 
+    String DamageID;
+    String VehicleID;
+    String VehiclePlateNo;
+    String DamageType;
+    String VehiclePhoto;
+    String Model;
 
 }
