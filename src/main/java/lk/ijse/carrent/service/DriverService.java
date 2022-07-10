eb253e128327963991959d4c154894194cb3d879
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-10 22:24:41 +0530

    created driver interface

---
+++ src/main/java/lk/ijse/carrent/service/DriverService.java
@@ -0,0 +1,19 @@
+package lk.ijse.carrent.service;
+
+import lk.ijse.carrent.dto.BillingDTO;
+import lk.ijse.carrent.dto.DriverDTO;
+
+import java.util.List;
+
+public interface DriverService {
+
+    void saveDriver(DriverDTO dto);
+
+    void deleteDriver(String id);
+
+    void updateDriver(String dto);
+
+    DriverDTO searchDriver(String id);
+
+    List<DriverDTO> getAllDriver();
+}
