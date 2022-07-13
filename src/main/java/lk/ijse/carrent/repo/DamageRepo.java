977435f2fa926cae128665afc82f6a7ca8e73a12
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:05:01 +0530

    created damageRepo interface

---
+++ src/main/java/lk/ijse/carrent/repo/DamageRepo.java
@@ -0,0 +1,9 @@
+package lk.ijse.carrent.repo;
+
+import lk.ijse.carrent.entity.Damage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DamageRepo extends JpaRepository<Damage,String> {
+
+}
