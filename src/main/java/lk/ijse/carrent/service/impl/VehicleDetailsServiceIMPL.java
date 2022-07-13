14af95b94da94a4377bfc5a5dc7117d08deacd0d
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:45:03 +0530

    created vehicledetailsIMPL autowired annotation

--- src/main/java/lk/ijse/carrent/service/impl/VehicleDetailsServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/VehicleDetailsServiceIMPL.java
@@ -1,6 +1,14 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.repo.VehicleDetailsRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class VehicleDetailsServiceIMPL {
 
+    @Autowired
+    private VehicleDetailsRepo repo;
 
+    @Autowired
+    private ModelMapper map;
 }
