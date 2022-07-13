cb467e0cbb420c9162ab82c282b9187e1d8a804f
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:40:24 +0530

    created customerIMPL autowired annotation

--- src/main/java/lk/ijse/carrent/service/impl/CustomerServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/CustomerServiceIMPL.java
@@ -1,6 +1,14 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.repo.CustomerRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class CustomerServiceIMPL {
 
+    @Autowired
+    private CustomerRepo repo;
 
+    @Autowired
+    private ModelMapper map;
 }
