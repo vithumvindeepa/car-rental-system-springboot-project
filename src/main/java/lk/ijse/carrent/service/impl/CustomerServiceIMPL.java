d66c83b648a08171a63d820bb13864ea13b21ceb
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:58:06 +0530

    created customerIMPL save method

--- src/main/java/lk/ijse/carrent/service/impl/CustomerServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/CustomerServiceIMPL.java
@@ -1,5 +1,9 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.dto.BillingDTO;
+import lk.ijse.carrent.dto.CustomerDTO;
+import lk.ijse.carrent.entity.Billing;
+import lk.ijse.carrent.entity.Customer;
 import lk.ijse.carrent.repo.CustomerRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,4 +15,15 @@ public class CustomerServiceIMPL {
 
     @Autowired
     private ModelMapper map;
+
+    public void saveCustomer(CustomerDTO dto) {
+
+        if (!repo.existsById(dto.getCustomerID())) {
+            repo.save(map.map(dto, Customer.class));
+        } else {
+            throw new RuntimeException("This Customer ID id is already exists....!!!");
+        }
+    }
+
+
 }
