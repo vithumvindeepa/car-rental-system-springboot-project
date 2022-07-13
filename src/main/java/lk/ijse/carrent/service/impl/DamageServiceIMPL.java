1bfb88b53e7e7adedab5a6b8891922a7a703db2a
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 04:02:28 +0530

    created damageIMPL save method

--- src/main/java/lk/ijse/carrent/service/impl/DamageServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/DamageServiceIMPL.java
@@ -1,5 +1,9 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.dto.BillingDTO;
+import lk.ijse.carrent.dto.DamageDTO;
+import lk.ijse.carrent.entity.Billing;
+import lk.ijse.carrent.entity.Damage;
 import lk.ijse.carrent.repo.DamageRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,4 +15,15 @@ public class DamageServiceIMPL {
 
     @Autowired
     private ModelMapper map;
+
+    public void saveDamage(DamageDTO dto) {
+
+        if (!repo.existsById(dto.getDamageID())) {
+            repo.save(map.map(dto, Damage.class));
+        } else {
+            throw new RuntimeException("This Damage ID id is already exists....!!!");
+        }
+    }
+
+
 }
