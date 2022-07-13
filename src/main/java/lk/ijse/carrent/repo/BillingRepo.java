bd67b2836a0daa0bb715a1ee428fc29d75d172e3
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 02:41:47 +0530

    billing impl update

---
+++ src/main/java/lk/ijse/carrent/repo/BillingRepo.java
@@ -0,0 +1,9 @@
+package lk.ijse.carrent.repo;
+
+import lk.ijse.carrent.entity.Billing;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BillingRepo extends JpaRepository<Billing,String> {
+
+
+}
--- src/main/java/lk/ijse/carrent/service/impl/BillingServicelMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/BillingServicelMPL.java
@@ -1,5 +1,59 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.dto.BillingDTO;
+import lk.ijse.carrent.entity.Billing;
+import lk.ijse.carrent.repo.BillingRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class BillingServicelMPL {
 
+    @Autowired
+    private BillingRepo repo;
+
+    @Autowired
+    private ModelMapper map;
+    public void saveBilling(BillingDTO dto) {
+
+        if (!repo.existsById(dto.getBillID())) {
+            repo.save(map.map(dto, Billing.class));
+        } else {
+            throw new RuntimeException("This Billing ID id is already exists....!!!");
+        }
+    }
+
+    public void deleteBilling(String id) {
+        if (repo.existsById(id)) {
+            repo.deleteById(id);
+        } else {
+            throw new RuntimeException("Please check the Billing ID.. No Such Billing..!");
+        }
+
+
+    }
+
+    public void updateBilling(BillingDTO dto) {
+        if (repo.existsById(dto.getBillID())) {
+            repo.save(map.map(dto, Billing.class));
+        } else {
+            throw new RuntimeException("No Such Billing To Update..! Please Check the ID!");
+        }
+    }
+
+    public BillingDTO searchBilling(String id) {
+        if (repo.existsById(id)){
+            return map.map(repo.findById(id).get(),BillingDTO.class);
+        }else {
+            throw new RuntimeException("No Billing For " + id + " ..!");
+        }
+
+    }
+
+    public List<BillingDTO> getAllBilling() {
+        return map.map(repo.findAll(), new TypeToken<List<BillingDTO>>() {
+        }.getType());
+    }
 }
