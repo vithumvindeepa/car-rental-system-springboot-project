5cdef06f053fd6de08527b9c3fd39cdc7a28d5de
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 04:11:22 +0530

    created vehicledetailsIMPL save method

--- src/main/java/lk/ijse/carrent/service/impl/VehicleDetailsServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/VehicleDetailsServiceIMPL.java
@@ -1,5 +1,7 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.dto.VehicledetailsDTO;
+import lk.ijse.carrent.entity.Vehicledetails;
 import lk.ijse.carrent.repo.VehicleDetailsRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,4 +13,15 @@ public class VehicleDetailsServiceIMPL {
 
     @Autowired
     private ModelMapper map;
+
+    public void saveVehicleDetails(VehicledetailsDTO dto) {
+
+        if (!repo.existsById(dto.getVehicleID())) {
+            repo.save(map.map(dto, Vehicledetails.class));
+        } else {
+            throw new RuntimeException("This VehicleDetails ID id is already exists....!!!");
+        }
+    }
+
+
 }
