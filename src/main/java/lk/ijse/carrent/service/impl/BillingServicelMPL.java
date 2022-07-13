0189b218e249992e78782be38ade50fadf5013ed
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:54:12 +0530

    created bookingIMPL save method

--- src/main/java/lk/ijse/carrent/service/impl/BillingServicelMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/BillingServicelMPL.java
@@ -16,6 +16,7 @@ public class BillingServicelMPL {
 
     @Autowired
     private ModelMapper map;
+
     public void saveBilling(BillingDTO dto) {
 
         if (!repo.existsById(dto.getBillID())) {
@@ -26,16 +27,16 @@ public class BillingServicelMPL {
     }
 
     public void deleteBilling(String id) {
+
         if (repo.existsById(id)) {
             repo.deleteById(id);
         } else {
             throw new RuntimeException("Please check the Billing ID.. No Such Billing..!");
         }
-
-
     }
 
     public void updateBilling(BillingDTO dto) {
+
         if (repo.existsById(dto.getBillID())) {
             repo.save(map.map(dto, Billing.class));
         } else {
@@ -44,15 +45,16 @@ public class BillingServicelMPL {
     }
 
     public BillingDTO searchBilling(String id) {
+
         if (repo.existsById(id)){
             return map.map(repo.findById(id).get(),BillingDTO.class);
         }else {
             throw new RuntimeException("No Billing For " + id + " ..!");
         }
-
     }
 
     public List<BillingDTO> getAllBilling() {
+
         return map.map(repo.findAll(), new TypeToken<List<BillingDTO>>() {
         }.getType());
     }
--- src/main/java/lk/ijse/carrent/service/impl/BooklingServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/BooklingServiceIMPL.java
@@ -1,4 +1,8 @@
 package lk.ijse.carrent.service.impl;
+import lk.ijse.carrent.dto.BillingDTO;
+import lk.ijse.carrent.dto.BookingDTO;
+import lk.ijse.carrent.entity.Billing;
+import lk.ijse.carrent.entity.Booking;
 import lk.ijse.carrent.repo.BookingRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,5 +13,16 @@ public class BooklingServiceIMPL {
 
     @Autowired
     private ModelMapper map;
+
+    public void saveBooking(BookingDTO dto) {
+
+        if (!repo.existsById(dto.getBookingID())) {
+            repo.save(map.map(dto, Booking.class));
+        } else {
+            throw new RuntimeException("This Booking ID id is already exists....!!!");
+        }
+    }
+
+
 
 }
