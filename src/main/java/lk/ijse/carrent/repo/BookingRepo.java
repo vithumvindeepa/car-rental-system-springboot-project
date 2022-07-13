066d2ff8c3181ecda9f051edf8acf71a8769c04a
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:03:01 +0530

    created customerRepo interface

--- src/main/java/lk/ijse/carrent/repo/BookingRepo.java
+++ src/main/java/lk/ijse/carrent/repo/BookingRepo.java
@@ -4,6 +4,7 @@ package lk.ijse.carrent.repo;
 import lk.ijse.carrent.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface BookingRepo extends JpaRepository<Booking,String> {
 
 }
---
+++ src/main/java/lk/ijse/carrent/repo/CustomerRepo.java
@@ -0,0 +1,9 @@
+package lk.ijse.carrent.repo;
+
+import lk.ijse.carrent.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CustomerRepo extends JpaRepository<Customer,String> {
+
+}
