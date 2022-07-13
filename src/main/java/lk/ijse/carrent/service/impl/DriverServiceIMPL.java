b7dd52394adabef525a89e3ce7a1b4cae1a3e3b5
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:42:45 +0530

    created driverIMPL autowired annotation

--- src/main/java/lk/ijse/carrent/service/impl/DriverServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/DriverServiceIMPL.java
@@ -1,6 +1,14 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.repo.DriverRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DriverServiceIMPL {
 
+    @Autowired
+    private DriverRepo repo;
 
+    @Autowired
+    private ModelMapper map;
 }
