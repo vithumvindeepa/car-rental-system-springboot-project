0ebf849fb0c0673d194b5e3dffe40daa07e0c9ed
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:00:11 +0530

    created bookingrepo interface

---
+++ src/main/java/lk/ijse/carrent/repo/BookingRepo.java
@@ -0,0 +1,9 @@
+package lk.ijse.carrent.repo;
+
+
+import lk.ijse.carrent.entity.Booking;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookingRepo extends JpaRepository<Booking,String> {
+
+}
