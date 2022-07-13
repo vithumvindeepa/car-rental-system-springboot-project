527acfa820e89b9e4dd714bbf4eaf7026bee7985
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:38:15 +0530

    created bookingIMPL autowired annotation

--- src/main/java/lk/ijse/carrent/service/impl/BooklingServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/BooklingServiceIMPL.java
@@ -1,6 +1,13 @@
 package lk.ijse.carrent.service.impl;
+import lk.ijse.carrent.repo.BookingRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class BooklingServiceIMPL {
+    @Autowired
+    private BookingRepo repo;
 
+    @Autowired
+    private ModelMapper map;
 
 }
