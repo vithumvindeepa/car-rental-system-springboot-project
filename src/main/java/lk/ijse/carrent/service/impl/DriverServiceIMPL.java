7c12b7318fa3d6b1604f460fe75c2d3cbb5a62fe
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 04:05:44 +0530

    created driverIMPL save method

--- src/main/java/lk/ijse/carrent/service/impl/DriverServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/DriverServiceIMPL.java
@@ -1,5 +1,9 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.dto.BillingDTO;
+import lk.ijse.carrent.dto.DriverDTO;
+import lk.ijse.carrent.entity.Billing;
+import lk.ijse.carrent.entity.Driver;
 import lk.ijse.carrent.repo.DriverRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,4 +15,15 @@ public class DriverServiceIMPL {
 
     @Autowired
     private ModelMapper map;
+
+    public void saveDriver(DriverDTO dto) {
+
+        if (!repo.existsById(dto.getDriverID())) {
+            repo.save(map.map(dto, Driver.class));
+        } else {
+            throw new RuntimeException("This Driver ID id is already exists....!!!");
+        }
+    }
+
+
 }
