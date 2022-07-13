168fc0e19af9d95a594a76fc6d69e70e28268c5d
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 04:07:33 +0530

    created employeeIMPL save method

--- src/main/java/lk/ijse/carrent/service/impl/EmployeeServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/EmployeeServiceIMPL.java
@@ -1,5 +1,9 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.dto.BillingDTO;
+import lk.ijse.carrent.dto.EmployeeDTO;
+import lk.ijse.carrent.entity.Billing;
+import lk.ijse.carrent.entity.Employee;
 import lk.ijse.carrent.repo.EmployeeRepo;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,4 +15,15 @@ public class EmployeeServiceIMPL {
 
     @Autowired
     private ModelMapper map;
+
+    public void saveEmployee(EmployeeDTO dto) {
+
+        if (!repo.existsById(dto.getEmployeeID())) {
+            repo.save(map.map(dto, Employee.class));
+        } else {
+            throw new RuntimeException("This Employee ID id is already exists....!!!");
+        }
+    }
+
+
 }
