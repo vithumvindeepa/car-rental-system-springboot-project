bbc19ff81cd0f80b30bd7488e1bc8ffb74efe152
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:43:48 +0530

    created employeeIMPL autowired annotation

--- src/main/java/lk/ijse/carrent/service/impl/EmployeeServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/EmployeeServiceIMPL.java
@@ -1,6 +1,14 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.repo.EmployeeRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class EmployeeServiceIMPL {
 
+    @Autowired
+    private EmployeeRepo repo;
 
+    @Autowired
+    private ModelMapper map;
 }
