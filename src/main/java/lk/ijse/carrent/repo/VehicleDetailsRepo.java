cc3ab7c0e04d2b8d2d3eaa0d5d01722051352874
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:11:59 +0530

    created vehicledetailsRepo interface

---
+++ src/main/java/lk/ijse/carrent/repo/VehicleDetailsRepo.java
@@ -0,0 +1,10 @@
+package lk.ijse.carrent.repo;
+
+
+import lk.ijse.carrent.entity.Vehicledetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface VehicleDetailsRepo extends JpaRepository<Vehicledetails,String> {
+
+}
