5029f07856cb39bba51413a5dde225c02dfcf416
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:41:27 +0530

    created damageIMPL autowired annotation

--- src/main/java/lk/ijse/carrent/service/impl/DamageServiceIMPL.java
+++ src/main/java/lk/ijse/carrent/service/impl/DamageServiceIMPL.java
@@ -1,6 +1,14 @@
 package lk.ijse.carrent.service.impl;
 
+import lk.ijse.carrent.repo.DamageRepo;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class DamageServiceIMPL {
 
+    @Autowired
+    private DamageRepo repo;
 
+    @Autowired
+    private ModelMapper map;
 }
