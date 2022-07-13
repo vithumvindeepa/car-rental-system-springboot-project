abd60113ebc45d1fd71cec3272e09a34764f7639
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-14 03:07:22 +0530

    created driverRepo interface

---
+++ src/main/java/lk/ijse/carrent/repo/DriverRepo.java
@@ -0,0 +1,11 @@
+package lk.ijse.carrent.repo;
+
+
+import lk.ijse.carrent.entity.Driver;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DriverRepo extends JpaRepository<Driver,String> {
+
+
+}
