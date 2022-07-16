f65e0c21c2742c4b3e53bdee980d0dde1379b160
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-16 21:13:52 +0530

    created Employeecontroller save method

--- src/main/java/lk/ijse/carrent/controller/EmployeeController.java
+++ src/main/java/lk/ijse/carrent/controller/EmployeeController.java
@@ -1,6 +1,31 @@
 package lk.ijse.carrent.controller;
 
+import lk.ijse.carrent.dto.EmployeeDTO;
+import lk.ijse.carrent.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import util.ResponceUtil;
+
+@RestController
+@RequestMapping("Employee")
+@CrossOrigin
+
 public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponceUtil SaveEmployee(EmployeeDTO employeedto) {
+        employeeService.saveEmployee(employeedto);
+        return new ResponceUtil(200, "save", null);
+
+    }
+
+
 
 
 }
