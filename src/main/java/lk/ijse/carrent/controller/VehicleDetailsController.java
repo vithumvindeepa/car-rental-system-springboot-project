3af53b808fdf041bbc2ffe76f6a1d658a1f6507d
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-16 21:35:49 +0530

    created VehicleDetailscontroller save method

--- src/main/java/lk/ijse/carrent/controller/VehicleDetailsController.java
+++ src/main/java/lk/ijse/carrent/controller/VehicleDetailsController.java
@@ -1,6 +1,33 @@
 package lk.ijse.carrent.controller;
 
+import lk.ijse.carrent.dto.CustomerDTO;
+import lk.ijse.carrent.dto.VehicledetailsDTO;
+import lk.ijse.carrent.service.VehicleDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import util.ResponceUtil;
+
+@RestController
+@RequestMapping("VehicleDetails")
+@CrossOrigin
+
 public class VehicleDetailsController {
+    @Autowired
+    VehicleDetailsService vehicleDetailsService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponceUtil SaveVehicleDetails(VehicledetailsDTO vehicledetailsDTO) {
+        vehicleDetailsService.saveVehicleDetails(vehicledetailsDTO);
+        return new ResponceUtil(200, "save", null);
+
+    }
+
+
+
 
 
 }
