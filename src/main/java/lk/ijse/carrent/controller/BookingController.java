40ad5e2bb2e47118ed874e22556707d0b5638d4f
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-16 20:39:50 +0530

    created Bookingcontroller save method

--- src/main/java/lk/ijse/carrent/controller/BookingController.java
+++ src/main/java/lk/ijse/carrent/controller/BookingController.java
@@ -1,6 +1,32 @@
 package lk.ijse.carrent.controller;
 
+import lk.ijse.carrent.dto.BookingDTO;
+import lk.ijse.carrent.dto.CustomerDTO;
+import lk.ijse.carrent.service.BillingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import util.ResponceUtil;
+
+@RestController
+@RequestMapping("Booking")
+@CrossOrigin
+
 public class BookingController {
+    @Autowired
+    BillingService bookingService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponceUtil SaveBooking(BookingDTO bookingdto) {
+        billingService.saveBooking(bookingdto);
+        return new ResponceUtil(200, "save", null);
+
+    }
+
+
 
 
 }
