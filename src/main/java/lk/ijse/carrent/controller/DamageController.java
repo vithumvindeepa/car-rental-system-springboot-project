cc91764e401cef09030b60cf9fcba436f55954fa
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-16 21:04:43 +0530

    created Damagecontroller save method

--- src/main/java/lk/ijse/carrent/controller/DamageController.java
+++ src/main/java/lk/ijse/carrent/controller/DamageController.java
@@ -1,10 +1,14 @@
 package lk.ijse.carrent.controller;
 
+import lk.ijse.carrent.dto.DamageDTO;
 import lk.ijse.carrent.service.DamageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import util.ResponceUtil;
 
 @RestController
 @RequestMapping("Damage")
@@ -14,6 +18,12 @@ public class DamageController {
     @Autowired
     DamageService damageService;
 
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponceUtil SaveDamage(DamageDTO damagedto) {
+        damageService.saveDamage(damagedto);
+        return new ResponceUtil(200, "save", null);
+
+    }
 
 
 
