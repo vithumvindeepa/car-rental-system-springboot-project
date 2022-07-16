f82b8757e45b5ae0fff678bed6fd452308d33b1d
vithumvindeepa <vindeepavithum@gmail.com>
2022-07-16 20:32:49 +0530

    created Billingcontroller save method

--- src/main/java/lk/ijse/carrent/controller/BillingController.java
+++ src/main/java/lk/ijse/carrent/controller/BillingController.java
@@ -1,6 +1,33 @@
 package lk.ijse.carrent.controller;
 
+import lk.ijse.carrent.dto.BillingDTO;
+import lk.ijse.carrent.dto.CustomerDTO;
+import lk.ijse.carrent.service.BillingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import util.ResponceUtil;
+
+@RestController
+@RequestMapping("Billing")
+@CrossOrigin
+
 public class BillingController {
+    @Autowired
+    BillingService billingService;
+
+    @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponceUtil SaveBilling(BillingDTO billingdto) {
+        billingService.saveBilling(billingdto);
+        return new ResponceUtil(200, "save", null);
+
+    }
+
+
+
 
 
 }
